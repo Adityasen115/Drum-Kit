fd667d47c4fc878eaf65db1cce279b2728f8e837
Adityasen115 <115805859+Adityasen115@users.noreply.github.com>
2022-11-16 20:51:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+for(var i=0;i<document.querySelectorAll("button").length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click" , function(){
+        var buttonInnerHTML = this.innerHTML;
+        makesound(buttonInnerHTML);
+    });
+}
+
+document.addEventListener("keypress", function(event){
+    makesound(event.key);
+});
+
+function makesound(key){
+    if(key=='w'){
+        var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+    }
+    else if(key=='a'){
+        var audio = new Audio('sounds/tom-2.mp3');
+    audio.play();
+    }
+    else if(key=='s'){
+        var audio = new Audio('sounds/tom-3.mp3');
+    audio.play();
+    }
+    else if(key=='d'){
+        var audio = new Audio('sounds/tom-4.mp3');
+    audio.play();
+    }
+    else if(key=='j'){
+        var audio = new Audio('sounds/snare.mp3');
+    audio.play();
+    }
+    else if(key=='k'){
+        var audio = new Audio('sounds/crash.mp3');
+    audio.play();
+    }
+    else if(key=='l'){
+        var audio = new Audio('sounds/kick-bass.mp3');
+    audio.play();
+    }
+    else
+    alert("This is the wrong input");
+}
